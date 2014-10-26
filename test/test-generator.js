ab1aac9a1f165d2f0b668c5753bca892ca82fd18
mrbarbasa <mrbarbasa@gmail.com>
2014-10-25 15:37:35 -1000

    Finish test-generator

--- test/test-generator.js
+++ test/test-generator.js
@@ -44,18 +44,70 @@ describe('MultidimensionalArray.generate', function() {
 
     it('each item inside the array should be a number, either 1 or 0', function() {
       var array = MDA.generate(4);
-      array.forEach(function(a, index) {
+      array.forEach(function(a) {
         expect(a).to.be.a('number');
       });
     });
   }); // end called with one argument
 
   describe('called with two arguments', function() {
+    it('should return a top-level array of length equivalent to the first number argument', function() {
+      expect(MDA.generate(5, 4).length).to.equal(5);
+      expect(MDA.generate(10, 3).length).to.equal(10);
+    });
 
+    it('each item inside the top-level array should be an array of length equivalent to the second number argument', function() {
+      var array = MDA.generate(5, 4);
+      array.forEach(function(a) {
+        expect(a).to.be.an('array');
+        expect(a.length).to.equal(4);
+      });
+    });
+
+    it('each item inside each second-level array should be a number, either 1 or 0', function() {
+      var array = MDA.generate(5, 4);
+      array.forEach(function(a) {
+        a.forEach(function(b) {
+          expect(b).to.be.a('number');
+        });
+      });
+    });
   }); // end called with two arguments
 
   describe('called with three arguments', function() {
+    it('should return a top-level array of length equivalent to the first number argument', function() {
+      expect(MDA.generate(3, 4, 5).length).to.equal(3);
+      expect(MDA.generate(5, 4, 3).length).to.equal(5);
+    });
 
+    it('each item inside the top-level array should be an array of length equivalent to the second number argument', function() {
+      var array = MDA.generate(3, 4, 5);
+      array.forEach(function(a) {
+        expect(a).to.be.an('array');
+        expect(a.length).to.equal(4);
+      });
+    });
+
+    it('each item inside the second-level array should be an array of length equivalent to the third number argument', function() {
+      var array = MDA.generate(3, 4, 5);
+      array.forEach(function(a) {
+        a.forEach(function(b) {
+          expect(b).to.be.an('array');
+          expect(b.length).to.equal(5);
+        });
+      });
+    });
+
+    it('each item inside each third-level array should be a number, either 1 or 0', function() {
+      var array = MDA.generate(3, 4, 5);
+      array.forEach(function(a) {
+        a.forEach(function(b) {
+          b.forEach(function(c) {
+            expect(c).to.be.a('number');
+          });
+        });
+      });
+    });
   }); // end called with three arguments
 
 });
